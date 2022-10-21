43b7ec5883b75e64d997ad01f1a346be81d0b8e2
Vinay <vinayaksingh920@gmail.com>
2022-10-21 23:44:16 +0530

    Started new project Portfolio Website

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const navbar = () => {
+  return (
+    <div className='container'>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+      <p>s</p>
+    </div>
+  )
+}
+
+export default navbar
